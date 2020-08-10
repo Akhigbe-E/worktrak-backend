b48448dee9ab91a79988a8eceeed4e7f384d19e3
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-10 18:12:41 +0100

    change DB URI

--- knex.js
+++ knex.js
@@ -5,7 +5,8 @@ const knex = require('knex')
 //Initialise knex
 const db = new knex({
     client: "pg",
-    connection: 'postgres://postgres:12345678910@localhost:5432/worktrak'
+    connection: process.env.DATABASE_URL
+    // connection: 'postgres://postgres:12345678910@localhost:5432/worktrak'
 })
 
 module.exports = db
